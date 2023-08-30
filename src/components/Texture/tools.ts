12eafa6b17e29b2bdeb2f5b94a9c403a7451d698
一晌贪欢 <53244269+3000-codes@users.noreply.github.com>
2023-08-30 22:04:36 +0800

    fix: remove deprecated apis

--- src/components/Texture/tools.ts
+++ src/components/Texture/tools.ts
@@ -1,24 +1,24 @@
 export function initCanvas(canvas: HTMLCanvasElement, width = 400, height = 400, _dpi?: number) {
-    const ctx = canvas.getContext('2d')!
-  
-    const dpr = window.devicePixelRatio || 1
-    const bsr = // 安装 hidpi-canvas
-      ctx.webkitBackingStorePixelRatio ||
-      ctx.mozBackingStorePixelRatio ||
-      ctx.msBackingStorePixelRatio ||
-      ctx.oBackingStorePixelRatio ||
-      ctx.backingStorePixelRatio ||
-      1
-  
-    const dpi = _dpi || dpr / bsr
-  
-    canvas.style.width = `${width}px`
-    canvas.style.height = `${height}px`
-    canvas.width = dpi * width
-    canvas.height = dpi * height
-    ctx.scale(dpi, dpi)
-  
-    return { ctx, dpi }
+  const ctx = canvas.getContext('2d')!
+
+  const dpr = window.devicePixelRatio || 1
+  const bsr = // 安装 hidpi-canvas
+    // ctx.webkitBackingStorePixelRatio ||
+    // ctx.mozBackingStorePixelRatio ||
+    // ctx.msBackingStorePixelRatio ||
+    // ctx.oBackingStorePixelRatio ||
+    // ctx.backingStorePixelRatio ||
+    1
+
+  const dpi = _dpi || dpr / bsr
+
+  canvas.style.width = `${width}px`
+  canvas.style.height = `${height}px`
+  canvas.width = dpi * width
+  canvas.height = dpi * height
+  ctx.scale(dpi, dpi)
+
+  return { ctx, dpi }
 }
 
 export function polar2cart(x = 0, y = 0, r = 0, theta = 0) {
@@ -27,10 +27,10 @@ export function polar2cart(x = 0, y = 0, r = 0, theta = 0) {
   return [x + dx, y + dy]
 }
 
-export type Fn = () => void 
+export type Fn = () => void
 
 export type Controls = {
   pause: Fn;
   resume: Fn;
 }
-  
+
